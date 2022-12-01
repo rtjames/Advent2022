9ac0252b9afe75d5a262d28ec6aa55e0b46d0b38
Robert James <jamest699@gmail.com>
2022-12-01 11:37:02 -0500

    Fixed Day1 formatting

--- lib/calorie_count.rb
+++ lib/calorie_count.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'advent_utils'
 
 DAY1 = 'day1.txt'
@@ -7,18 +9,18 @@ input = get_input_file(DAY1)
 # array of integers
 calorie_counts = Array.new(1, 0)
 
-while not input.eof
-    line = input.readline.chomp
-    if line.empty?
-        calorie_counts << 0
-    else
-        calories = Integer(line)
-        calorie_counts[-1] += calories
-    end
+until input.eof
+  line = input.readline.chomp
+  if line.empty?
+    calorie_counts << 0
+  else
+    calories = Integer(line)
+    calorie_counts[-1] += calories
+  end
 end
 
 # Part 1: 74198
-puts "Part 1: #{ calorie_counts.max }"
+puts "Part 1: #{calorie_counts.max}"
 
 # Part 2: 209914
-puts "Part 2: #{ calorie_counts.max(3).sum }"
+puts "Part 2: #{calorie_counts.max(3).sum}"
