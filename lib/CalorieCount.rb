1f59096fbd3e4d3ab5cb1f7b13abd1381dc94f34
Robert James <jamest699@gmail.com>
2022-12-01 09:53:50 -0500

    Add comments to Day 1

--- lib/CalorieCount.rb
+++ lib/CalorieCount.rb
@@ -2,6 +2,7 @@ DAY_ONE_PATH = '../data/day1.txt'
 
 input = File.open(DAY_ONE_PATH, "r")
 
+# array of arrays of integers
 food_inventory = Array.new(1, Array.new)
 
 while not input.eof
@@ -14,9 +15,11 @@ while not input.eof
     end
 end
 
+# array of integers
 calorie_counts = food_inventory.map { |calories| calories.sum }
 
 # Part 1: 74198
 puts "Part 1: #{calorie_counts.max}"
 
+# Part 2: 209914
 puts "Part 2: #{calorie_counts.max(3).sum}"
