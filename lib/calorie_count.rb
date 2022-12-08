8eb1465bab1b2dcb61320307373f3797bc84e0f8
Robert.James <Robert.James>
2022-12-08 15:53:06 -0500

    Add CLI tool for running slns; add day 1 to CLI

--- lib/calorie_count.rb
+++ lib/calorie_count.rb
@@ -1,26 +1,16 @@
-# frozen_string_literal: true
+def sum_max_calorie_counts(input, counts_num = 1)
+  # array of integers
+  calorie_counts = Array.new(1, 0)
 
-require_relative 'advent_utils'
-
-DAY1 = 'day1.txt'
-
-input = get_input_file(DAY1)
-
-# array of integers
-calorie_counts = Array.new(1, 0)
-
-until input.eof
-  line = input.readline.chomp
-  if line.empty?
-    calorie_counts << 0
-  else
-    calories = Integer(line)
-    calorie_counts[-1] += calories
+  until input.eof
+    line = input.readline.chomp
+    if line.empty?
+      calorie_counts << 0
+    else
+      calories = Integer(line)
+      calorie_counts[-1] += calories
+    end
   end
+
+  calorie_counts.max(counts_num).sum
 end
-
-# Part 1: 74198
-puts "Part 1: #{calorie_counts.max}"
-
-# Part 2: 209914
-puts "Part 2: #{calorie_counts.max(3).sum}"
---
+++ lib/main.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'pathname'
+
+# example input
+# ruby main.rb -day 1 -part 2 -input "./data/day1.txt"
+
+# Parameter Validation
+day_flag_index = ARGV.find_index('-day')
+if day_flag_index.nil?
+    raise 'Parameter required: -day'
+end
+
+day = ARGV[day_flag_index + 1]
+if day.nil? ||  day !~ /\A\d+\Z/
+    raise 'Invalid parameter: -day should be followed by an integer i.e. -day 1'
+end
+
+part_flag_index = ARGV.find_index('-part')
+if part_flag_index.nil?
+    raise 'Parameter required: -part'
+end
+
+part = ARGV[part_flag_index + 1]
+unless part == '1' || part == '2'
+    raise 'Invalid parameter: -part should be followed by 1 or 2 i.e. -part 1'
+end
+
+input_flag_index = ARGV.find_index('-input')
+if input_flag_index.nil?
+    raise 'Parameter required: -input'
+end
+
+input_path = ARGV[input_flag_index + 1]
+if input_path.nil? || (not File.exist?(input_path))
+    raise 'Invalid parameter: -input should be followed by a valid file path i.e. -input "./data/day1.txt"'
+end
+
+# Start of the happy path
+day = Integer day
+part = Integer part
+input = File.open(input_path, 'r')
+
+case day
+when 1
+    require_relative 'calorie_count.rb'
+    if part == 1
+        # 74198
+        answer = sum_max_calorie_counts(input)
+    elsif part == 2
+        # 209914
+        answer = sum_max_calorie_counts(input, 3)
+    end
+else
+    puts "Day #{day} Part #{part} has not been added"
+end
+
+puts "Day #{day} Part #{part}: #{answer}"
