f97a53b816844fe04b5e3b6e118a216e4bc72674
Robert.James <Robert.James>
2022-12-08 16:40:43 -0500

    Clean up main

--- lib/main.rb
+++ lib/main.rb
@@ -2,10 +2,36 @@
 
 require 'pathname'
 
+def solve_part(day, part, input)
+    case day
+    when 1
+        require_relative 'calorie_count.rb'
+        if part == 1
+            # 74198
+            answer = sum_max_calorie_counts(input)
+        elsif part == 2
+            # 209914
+            answer = sum_max_calorie_counts(input, 3)
+        end
+    when 2
+        require_relative 'rock_paper_scissors.rb'
+        if part == 1
+            # 12645
+            answer = calculate_match_scores(input)
+        elsif part == 2
+            # 11756
+            answer = calculate_match_scores(input, :two)
+        end
+    else
+        return "Day #{day} Part #{part} has not been added"
+    end
+    
+    "Day #{day} Part #{part}: #{answer}"
+end
+
 # example input
 # ruby main.rb -day 1 -part 2 -input "./data/day1.txt"
 
-# Parameter Validation
 day_flag_index = ARGV.find_index('-day')
 if day_flag_index.nil?
     raise 'Parameter required: -day'
@@ -36,32 +62,8 @@ if input_path.nil? || (not File.exist?(input_path))
     raise 'Invalid parameter: -input should be followed by a valid file path i.e. -input "./data/day1.txt"'
 end
 
-# Start of the happy path
 day = Integer day
 part = Integer part
 input = File.open(input_path, 'r')
 
-case day
-when 1
-    require_relative 'calorie_count.rb'
-    if part == 1
-        # 74198
-        answer = sum_max_calorie_counts(input)
-    elsif part == 2
-        # 209914
-        answer = sum_max_calorie_counts(input, 3)
-    end
-when 2
-    require_relative 'rock_paper_scissors.rb'
-    if part == 1
-        # 12645
-        answer = calculate_match_scores(input)
-    elsif part == 2
-        # 11756
-        answer = calculate_match_scores(input, :two)
-    end
-else
-    puts "Day #{day} Part #{part} has not been added"
-end
-
-puts "Day #{day} Part #{part}: #{answer}"
+puts solve_part(day, part, input)
